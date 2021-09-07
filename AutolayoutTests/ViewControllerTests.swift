0bb920295e9753680f0aa0bf1c14a832fc107a81
sesur <sergiusuru@ymail.com>
2021-09-07 09:32:35 +0300

    Create test class & setup test speed and covering

---
+++ AutolayoutTests/ViewControllerTests.swift
@@ -0,0 +1,14 @@
+//
+//  ViewControllerTests.swift
+//  AutolayoutTests
+//
+//  Created by Sergiu on 07.09.2021.
+//  Copyright © 2021 Sergiu. All rights reserved.
+//
+
+import XCTest
+@testable import Autolayout
+
+class ViewControllerTests: XCTestCase {
+    
+}
